2daa300f2d8f0c6eb202c0a00b58b4eeff97f2fe
mdasif8393 <md.asif.8393@gmail.com>
2023-01-28 23:36:06 +0600

    add filter using instock, amd, inter to get data

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,36 +1,72 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const filters = useSelector(state => state.filter.filters);
+  const {brands, stock} = filters;
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.data));
   }, []);
-
+  
   const activeClass = "text-white  bg-indigo-500 border-white";
+
+  let content;
+
+  if(products.length){
+    content = products.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
+  if(products.length && (stock || brands.length)){ //if product length, stock = true brand length true
+    content = products
+    .filter((product) => {
+      if(stock){
+        return product.status === true //if stock true then return product status true
+      }
+      return product;
+    })
+    .filter(product => {
+      if(brands.length){
+        return brands.includes(product.brand);
+      }
+      return product;
+    })
+    .map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
+
 
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
-        <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+        <button onClick={()=> dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null} `}
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={()=>dispatch(toggleBrand("amd"))} 
+         className={`border px-3 py-2 rounded-full font-semibold  ${brands.includes('amd') ? activeClass : null}`}>
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={()=>dispatch(toggleBrand("intel"))}
+         className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("intel") ? activeClass : null}`}>
           Intel
         </button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+        {
+          content
+        }
       </div>
     </div>
   );
